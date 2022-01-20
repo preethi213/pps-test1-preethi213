94c3a2d57fd1d17638ec6610b7cf9934d1eb9a42
preethi214 <97090251+preethi213@users.noreply.github.com>
2022-01-20 16:25:23 +0000

    v

--- p1original.c
+++ p1original.c
@@ -1,2 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  float sum;
+  printf("enter two number\n");
+  scanf("%d%d",&num1,&num2);
+  sum = num1+num2;
+  printf("addition of %d+%d is= %f\n",num2,num1,sum);
+  return 0;
 }
